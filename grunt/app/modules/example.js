d09c2ef5d8416d5d6af70ab7bde09d9cf9be102e
Drewz Lau <drewz@2mod5.com>
2012-07-06 09:50:44 -0700

    Initial checkin

---
+++ grunt/app/modules/example.js
@@ -0,0 +1,29 @@
+define([
+  "namespace",
+
+  // Libs
+  "use!backbone"
+
+  // Modules
+
+  // Plugins
+],
+
+function(namespace, Backbone) {
+
+  // Create a new module
+  var Example = namespace.module();
+
+  // This will fetch the tutorial template and render it.
+  Example.Views.Index = Backbone.View.extend({
+    template: "example",
+
+    serialize: function() {
+      return { name: "World" };
+    }
+  });
+
+  // Required, return the module for AMD compliance
+  return Example;
+
+});
